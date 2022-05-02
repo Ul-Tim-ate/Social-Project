bc4df7c23105cb99f6231277d8ba1e994d546d54
Timofey Trakhanov <timohator1@mail.ru>
2022-05-02 16:35:17 +0300

    connect firebase

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  async getHello() {
+			const db = admin.firestore();
+			const data = {
+				id: 1,
+				name: "Tima"
+			}
+			db.collection("Customers").doc(data.id.toString()).set(data);
+		}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import * as admin from 'firebase-admin';
+import { ServiceAccount } from "firebase-admin";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService: ConfigService = app.get(ConfigService);
+  // Set the config options
+  const adminConfig: ServiceAccount = {
+    "projectId": configService.get<string>('FIREBASE_PROJECT_ID'),
+    "privateKey": configService.get<string>('FIREBASE_PRIVATE_KEY')
+                               .replace(/\\n/g, '\n'),
+    "clientEmail": configService.get<string>('FIREBASE_CLIENT_EMAIL'),
+  };
+  // Initialize the firebase admin app
+  admin.initializeApp({
+    credential: admin.credential.cert(adminConfig),
+  });
+
+  app.enableCors();
+
+  await app.listen(4000);
 }
 bootstrap();
