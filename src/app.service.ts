82736e2f78a4f8d24c0400eddf123c0a9684da0a
Timofey Trakhanov <timohator1@mail.ru>
2022-05-02 16:37:28 +0300

    connect firebase

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,9 +6,13 @@ export class AppService {
   async getHello() {
 			const db = admin.firestore();
 			const data = {
-				id: 1,
-				name: "Tima"
+				id: 2,
+				name: "Dima"
 			}
 			db.collection("Customers").doc(data.id.toString()).set(data);
+			const snapshot = await db.collection('Customers').get();
+   snapshot.forEach((doc) => {
+   console.log(doc.id, '=>', doc.data());
+});
 		}
 }
